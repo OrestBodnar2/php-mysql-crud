0cbf07ff750ccc7fa2e41074895e6ee41d782b25
OrestBodnar2 <97770668+OrestBodnar2@users.noreply.github.com>
2022-09-07 14:47:12 +0300

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 session_start();
 
 $conn = mysqli_connect(
-  'phpmysqlcrud-server.mysql.database.azure.com',
+  'phpmysql-server.mysql.database.azure.com',
   'sqluser',
   'Testpass123',
   'php_mysql_crud'
